e6815d254d25ff1c0a1475880ac88db85e1da795
CTAN210 <CTAN210@e.ntu.edu.sg>
2021-10-11 14:17:30 +0800

    Updates

---
+++ precache-manifest.61192de3f0407c88f32f9730d4c8f1f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ff3ff0dca60dfe358cae0aca07d8d8",
+    "url": "/react-page/index.html"
+  },
+  {
+    "revision": "6bf2b500b08b32dd9de5",
+    "url": "/react-page/static/css/main.40c592de.chunk.css"
+  },
+  {
+    "revision": "40848d1207d7dbe0afb3",
+    "url": "/react-page/static/js/2.05d6a73f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-page/static/js/2.05d6a73f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf2b500b08b32dd9de5",
+    "url": "/react-page/static/js/main.8d664625.chunk.js"
+  },
+  {
+    "revision": "fa17e2964f049ec59a6e",
+    "url": "/react-page/static/js/runtime-main.f8ad19ce.js"
+  }
+]);
